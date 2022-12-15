9652e891fe707f85e9b794681777fed29990fe73
Gustavo Almeida de Jesus <66255218+SystemGuuh@users.noreply.github.com>
2022-12-15 12:28:00 -0300

    Coleta tell via requisição de login

--- BabyAlarm/BabyAlarmESP8266.ino
+++ BabyAlarm/BabyAlarmESP8266.ino
@@ -10,11 +10,10 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <string.h>
-#include <UrlEncode.h>
 
 // Configurações bot whatsapp
-String YOUR_PHONE = "5511967833245"; 
-String YOUR_APIKEY = "6975923"; //Your API KEY
+String YOUR_PHONE; 
+String YOUR_APIKEY; //Your API KEY
 String URL = "https://api.callmebot.com/whatsapp.php?phone=+";
 
 // Configurações de usuário
@@ -96,17 +95,10 @@ String posted() {
   String apikey=input("apikey");
   wifi_ssid = rede;
   wifi_password = password;
-  //YOUR_PHONE= celular; 
-  //YOUR_APIKEY= apikey;
+  YOUR_PHONE= celular; 
+  YOUR_APIKEY= apikey;
   Credentials="<li>Nome da rede: <b>" + rede + "</b></br>Senha: <b>" + password + "</b></li>" + Credentials;
   return header(POST_TITLE) + POST_BODY + footer();
-}
-
-String clear() {
-  String rede="<p></p>";
-  String password="<p></p>";
-  Credentials="<p></p>";
-  return header(CLEAR_TITLE) + "<div><p>A lista foi limpa.</div></p><center><a style=\"color:blue\" href=/>Volta ao inicio</a></center>" + footer();
 }
 
 
@@ -116,6 +108,8 @@ void setup_wifi() {
   Serial.println("");
   Serial.print("Conectando a ");
   Serial.println(wifi_ssid);
+  Serial.print("O telefone eh: ");
+  Serial.println(YOUR_PHONE);
   WiFi.hostname(WiFi_hostname);
   WiFi.begin(wifi_ssid, wifi_password);
 
@@ -132,25 +126,10 @@ void setup_wifi() {
 void sendWhatsapp(String text) {
   WiFiClient cliente;
   HTTPClient https;
-  String url = URL + YOUR_PHONE + "&text=" + text + "&apikey=" + YOUR_APIKEY;
-  //https://api.callmebot.com/whatsapp.php?phone=+5511967833245&text=Ola+aqui+eh+o+BabyAlarm.+Em+breve+mandarei+a+temperatura+do+seu+bebe,+se+ele+chorar+eu+aviso+tambem.&apikey=6975923
-  Serial.println(url);
+  Serial.println(text);
+  String url = URL + YOUR_PHONE + "&text=" + text + "&apikey=" + YOUR_APIKEY;Serial.println(url);
   https.begin(cliente,url);
   int httpCode = https.GET();
-
- // Specify content-type header
-  https.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  
-  // Send HTTP POST request
-  int httpResponseCode = https.POST(url);
-  if (httpResponseCode == 200){
-    Serial.print("Message sent successfully");
-  }
-  else{
-    Serial.println("Error sending the message");
-    Serial.print("HTTP response code: ");
-    Serial.println(httpResponseCode);
-  }
   
   https.end();
 }
@@ -164,7 +143,6 @@ void setup() {
   dnsServer.start(DNS_PORT, "*", APIP); // DNS spoofing (Apenas HTTP)
   webServer.on("/post",[]() { webServer.send(HTTP_CODE, "text/html", posted()); setup_wifi(); });
   webServer.on("/creds",[]() { webServer.send(HTTP_CODE, "text/html", creds()); });
-  webServer.on("/clear",[]() { webServer.send(HTTP_CODE, "text/html", clear()); });
   webServer.onNotFound([]() { lastActivity=millis(); webServer.send(HTTP_CODE, "text/html", index()); });
   webServer.begin();
   
@@ -174,8 +152,8 @@ void setup() {
 
 // Variaveis pro sensor de som e execuçao
 int mensagem=1;
-int sensorPin = 0;    // select the input pin for the potentiometer
-int sensorValue1 = 0;  // variable to store the value coming from the sensor
+int sensorPin = 0;    
+int sensorValue1 = 0;  
 int sensorValue2 = 0;
 int cont = 0;
 int variacao = 0;
@@ -191,7 +169,7 @@ dnsServer.processNextRequest(); webServer.handleClient();
   sensors.requestTemperatures(); 
   float temperatureC = sensors.getTempCByIndex(0);
   String temp = String(temperatureC);
-  String tempTXT = "Temperatura:+" + temp + "ºC";
+  String tempTXT = "Temperatura:%20" + temp + "ºC";
   Serial.println(tempTXT);
 
   //faz leitura de som
@@ -200,34 +178,34 @@ dnsServer.processNextRequest(); webServer.handleClient();
   variacao = (sensorValue1 - sensorValue2);
   Serial.println(variacao);
 
+  //manda mensagem de ola uma vez
   if(mensagem==1){
-    sendWhatsapp("Ola+aqui+eh+o+BabyAlarm.+Em+breve+mandarei+a+temperatura+do+seu+bebe,+se+ele+chorar+eu+aviso+tambem.");
-
+    sendWhatsapp(tempTXT);
+    sendWhatsapp("Ola%20aqui%20eh%20o%20BabyAlarm.%20Em%20breve%20mandarei%20a%20temperatura%20do%20seu%20bebe,%20se%20ele%20chorar%20eu%20aviso%20tambem.");
     mensagem=0;
   }
-    /*
-        //se temperatura baixa, avisa
-        if(temperatureC < 34.9-(34.9*0.1){
-          sendWhatsapp("O+bebe+esta+com+a+temperatura+baixa!!");
-          sendWhatsapp(tempTXT);
-        }else if(temperatureC > 36.9+(36.9*0.1)){
-          sendWhatsapp("O+bebe+esta+com+a+temperatura+alta!!");
-          sendWhatsapp(tempTXT);
-        }
-        
-        // Verifica se bebe esta chorando ha um tempo(aprox 1 minuto c/ delay de codigo)
-        if (variacao>1 || variacao<-1)  {
-          cont = cont+1;
-          if (cont>=4){
-            // BEBE CHORANDO
-            sendWhatsapp("O bixo ta choranu vei");
-            Serial.println("Seu bebê está chorando!");
-            cont = 0;
-          }
-        }
- }
- }
-*/
+    
+  //se temperatura baixa, avisa
+  if(temperatureC < 34.9-(34.9*0.1) && millis()>(1000*60*60)){
+    sendWhatsapp("O%20bebe%20esta%20com%20a%20temperatura%20baixa!!");
+    sendWhatsapp(tempTXT);
+  }else if(temperatureC > 36.9+(36.9*0.1) && millis()>(1000*60*60)){
+    sendWhatsapp("O%20bebe%20esta%20com%20a%20temperatura%20alta!!");
+    sendWhatsapp(tempTXT);
+  }
+  
+  // Verifica se bebe esta chorando ha um tempo(aprox 1 minuto c/ delay de codigo)
+  if (variacao>1 || variacao<-1)  {
+    cont = cont+1;
+    if (cont>=4){
+      // BEBE CHORANDO
+      sendWhatsapp("O%20seu%20bebe%20esta%20chorando.");
+      cont = 0;
+    }
+  }
+ 
+ 
+
   
   delay(1000);  
   }
